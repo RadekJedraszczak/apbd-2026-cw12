560562b5ea5a8ca16dee52b1d0960bf20b793367
RadekJedraszczak <s32847@pjwstk.edu.pl>
2026-05-21 10:00:16 +0200

    first save

---
+++ Hospital_Cf/DTOs/GetBedsDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Hospital_Cf.DTOs;
+
+public class GetBedsDto
+{
+    public int Id { get; set; }
+    public GetBedsTypesDto Type { get; set; }
+}
---
+++ Hospital_Cf/DTOs/GetWardsDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Hospital_Cf.DTOs;
+
+public class GetWardsDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+}
